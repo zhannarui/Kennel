09d4bfaf20d48009b11faabf7f57961d288c1ad8
zhannarui <zhannik2001@gmail.com>
2022-06-16 12:48:08 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -15,8 +15,17 @@ $('.mobile__menu-btn').on('click', function(e) {
     e.preventDefault();
     $('.menu__body').toggleClass('menu-active');   
     $('.mobile__menu-btn').toggleClass('menu-btn-active');
-})
-
+});
+let btnMenu = $(".mobile__menu-btn");
+let menu = $(".menu__body");
+$(document).click(function(e){
+if(!btnMenu.is(e.target)&&btnMenu.has(e.target).length === 0 && 
+!menu.is(e.target) && menu.has(e.target).length === 0
+){
+	menu.removeClass('menu-active');
+	btnMenu.removeClass('menu-btn-active');
+}
+});
 $('.aboutUs-text-btnMore').click(function(){
 	$('.aboutUs-text-more').slideToggle(500, function(){
 		if ($(this).is(':hidden')) {
